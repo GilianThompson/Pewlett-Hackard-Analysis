69d1301eab8dd58a96b1ee765119b0230dfc3095
GilianThompson <85901073+GilianThompson@users.noreply.github.com>
2021-08-05 00:16:55 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -41,16 +41,14 @@ CREATE TABLE dept_employees(
 	dept_no VARCHAR NOT NULL, 
 	from_date DATE NOT NULL, 
 	to_date DATE NOT NULL, 
-	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
-
+ 
 CREATE TABLE titles(
 	emp_no INT NOT NULL, 
 	title VARCHAR NOT NULL, 
 	from_date DATE NOT NULL, 
-	to_date DATE NOT NULL, 
-	PRIMARY KEY (emp_no), 
+	to_date DATE NOT NULL,  
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
